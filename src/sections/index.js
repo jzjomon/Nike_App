9db49e1feaa235c5aaa54f313f258448395146a1
jomon jose <jzjomon@gmail.com>
2023-09-18 14:51:51 +0530

    initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import Footer from './Footer';
+import CustomerReviews from './CustomerReviews';
+import PopularProducts from './PopularProducts';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import SupperQuality from './SupperQuality';
+import Subscribe from './Subscribe';
+
+export {
+    Hero,
+    PopularProducts,
+    Footer,
+    CustomerReviews,
+    Services,
+    SpecialOffer,
+    SupperQuality,
+    Subscribe
+}
